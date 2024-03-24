943d044ecbe8ef70d3d1df082f2818001297a62c
aliyeff-f <eliyevnurlan811@gmail.com>
2024-03-25 00:23:25 +0400

    finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,16 @@ for (let i = 0; i < navElems.length; i++) {
     overlay.classList.toggle("active");
   });
 }
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 80) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
